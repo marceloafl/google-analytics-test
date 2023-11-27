18ac75815a7265706d6fcd872aeb5cd908abc3fd
Marcelo Lemes <marceloaflemes@gmail.com>
2023-11-27 17:36:59 -0300

    adding Google Analytics tag

--- app/src/components/Analytics/Analytics.tsx
+++ app/src/components/Analytics/Analytics.tsx
@@ -1,27 +1,27 @@
-// import Script from 'next/script'
-// import React from 'react'
+import Script from 'next/script'
+import React from 'react'
 
-// function Analytics() {
-//   return (
-//     <>
-//       <Script
-//       // Replace NEXT_PUBLIC_GA_TRACKING at env.prd to link with the correct Measurement ID
-//         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
-//         strategy='afterInteractive'
-//       />
-//       <Script id='google-analytics' strategy='afterInteractive'>
-//         {`
-//           window.dataLayer = window.dataLayer || [];
-//           function gtag(){dataLayer.push(arguments);}
-//           gtag('js', new Date());
-//           gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}');
-//         `}
-//       </Script>
-//     </>
-//   )
-// }
+function Analytics() {
+  return (
+    <>
+      <Script
+      // Replace NEXT_PUBLIC_GA_TRACKING at env.prd to link with the correct Measurement ID
+        src={`https://www.googletagmanager.com/gtag/js?id=G-5MPNGX8WE5`}
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5MPNGX8WE5');
+        `}
+      </Script>
+    </>
+  )
+}
 
-// export default Analytics
+export default Analytics
 
 
 
